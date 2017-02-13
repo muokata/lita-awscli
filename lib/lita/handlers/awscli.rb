5e85d5cbf74dbd2162b125d30912cc3a3b8330b6
muokata <allanrobertgraham@gmail.com>
2017-02-13 11:29:33 -0500

    remove hardcode us-east-1. use ~/.aws/config instead. changed help

--- lib/lita/handlers/awscli.rb
+++ lib/lita/handlers/awscli.rb
@@ -4,13 +4,13 @@ module Lita
 
       # AwsCli handler
       
-      route(/^aws\s(\w+)/, :aws, help: { "aws " => "aws `prod`|`develop`|`build` returns running EC2 instances on those accounts/" })
+      route(/^aws\s(\w+)/, :aws, help: { "aws " => "aws `prod`|`oldprod`|`stage`|`dev` returns running EC2 instances on those accounts/" })
 
       def aws(response)
 
       	aws_profile = response.matches[0][0]
       	response.reply "Querying AWS For all EC2 Instances on profile #{aws_profile}"
-  	text = `aws --profile #{aws_profile} --region us-east-1 ec2 describe-instances --query "Reservations[].Instances[].{ ID:InstanceId, TYPE:InstanceType, ZONE:Placement.AvailabilityZone, KEY:KeyName, EC2Hostname:PublicDnsName, NAME:Tags[?Key=='Name'].Value | [0], ROLE:Tags[?Key=='Role'].Value | [0], PRIVATEIP:PrivateIpAddress STATE:State.Name}" --output table`
+  	text = `aws --profile #{aws_profile} ec2 describe-instances --query "Reservations[].Instances[].{ ID:InstanceId, TYPE:InstanceType, ZONE:Placement.AvailabilityZone, KEY:KeyName, EC2Hostname:PublicDnsName, NAME:Tags[?Key=='Name'].Value | [0], ROLE:Tags[?Key=='Role'].Value | [0], PRIVATEIP:PrivateIpAddress STATE:State.Name}" --output table`
 	# test = "account id was: #{aws_profile}"
   	response.reply("```#{text}```")
 
